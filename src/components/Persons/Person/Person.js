ec050425dc2702c2fd8916c29d144aba6df0bd15
fadhlu <fadhlurahman.i@gmail.com>
2020-07-02 15:55:24 +0700

    use hoc(high order component)

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
+import Aux from '../../../hoc/Aux';
 import classes from './Person.css';
 
 class Person extends Component {
   render() {
     console.log('[Person.js] rendering...');
     return (
-      <div className={classes.Person}>
+      <Aux>
         <p onClick={this.props.click}>
           I'm a {this.props.name} and i'm {this.props.age} years old
         </p>
         <p>{this.props.children}</p>
         <hr />
-        <input type="text" onChange={this.props.inputName} value={this.props.name} />
-      </div>
+        <input
+          type="text"
+          onChange={this.props.inputName}
+          value={this.props.name}
+        />
+      </Aux>
     );
   }
 }
