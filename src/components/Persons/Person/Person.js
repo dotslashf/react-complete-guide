d715acc59408d4bca7978b2a48634a8e462573fc
fadhlu <fadhlurahman.i@gmail.com>
2020-07-03 22:04:48 +0700

    use contextType for class component and useContext for functional component

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -12,18 +12,23 @@ class Person extends Component {
     this.inputElementRef = React.createRef();
   }
 
+  static contextType = AuthContext;
+
   componentDidMount() {
     // this.inputElement.focus();
     this.inputElementRef.current.focus();
+    console.log(this.context.authenticated);
   }
 
   render() {
     console.log('[Person.js] rendering...');
     return (
       <Aux>
-        <AuthContext.Consumer>
-          {(context) => context.authenticated ? <p>Authenticated!</p> : <p>Please login first!</p>}
-        </AuthContext.Consumer>
+        {this.context.authenticated ? (
+          <p>Authenticated!</p>
+        ) : (
+          <p>Please login first!</p>
+        )}
         <p onClick={this.props.click}>
           I'm a {this.props.name} and i'm {this.props.age} years old
         </p>
