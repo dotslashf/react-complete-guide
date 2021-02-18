0cc4625de225dd3215df38bd73fd5b600d170afa
fadhlu <fadhlurahman.i@gmail.com>
2021-02-18 10:35:13 +0700

    add sidedrawer toggler

---
+++ src/components/Navigation/SideDrawer/SideDrawerToggler/SideDrawerToggler.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const sideDrawerToggler = props => <div onClick={props.clicked}>Menu</div>;
+
+export default sideDrawerToggler;
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -3,10 +3,11 @@ import React from 'react';
 import classes from './Toolbar.css';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import SideDrawerToggler from '../SideDrawer/SideDrawerToggler/SideDrawerToggler';
 
 const toolbar = props => (
   <header className={classes.Toolbar}>
-    <div>Menu</div>
+    <SideDrawerToggler clicked={props.drawerToggleClick} />
     <div className={classes.Logo}>
       <Logo />
     </div>
